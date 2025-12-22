3444412cd6f5cdcb9db626347c9802b709a89a71
Dhairya Raniwal <dhairyaraniwal914@gmail.com>
2025-12-23 00:17:28 +0530

    minor fix

--- rajni/__init__.py
+++ rajni/__init__.py
@@ -5,10 +5,10 @@ This package contains the finalized inference-time pruning model,
 along with benchmarking, FLOPs analysis, and visualization utilities.
 
 Main entry points:
-- AdaptiveJacobianPrunedViT_DerivedRho
+- AdaptiveJacobianPrunedViT
 - benchmark
 """
 
-from .model import AdaptiveJacobianPrunedViT_DerivedRho
+from .model import AdaptiveJacobianPrunedViT
 from .benchmark import benchmark
 from .utils import print_token_counts
